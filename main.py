9c1d83e26e2b6746d4da67526d4687abc3b0fd1a
Nuro <136151272+nurology1@users.noreply.github.com>
2024-12-26 04:31:37 -0500

    v0.3T

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ from dotenv import load_dotenv
 load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
 
+# EVENTS
+
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user}')
 
     async def on_message(self, message):
-        print(f'Message Detected. "{message.author}: {message.content}"')
+        if message.author == self.user:
+            return
+        if message.content.startswith('hello'):
+            await message.channel.send(f'Hi there {message.author}!')
 
-
+# INTENTS
 intents = discord.Intents.default()
 intents.message_content = True
 
+# RUN
 
 client = Client(intents=intents)
 client.run(discord_token)
