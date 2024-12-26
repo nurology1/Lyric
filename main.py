2bc27ffb2b8c8b43ad1c7ff75f183bb4162bdbe8
Nuro <136151272+nurology1@users.noreply.github.com>
2024-12-26 03:11:02 -0500

    Update main.py
    Original Didn't Work v1.1T

--- main.py
+++ main.py
@@ -5,21 +5,13 @@ from dotenv import load_dotenv
 # TOKEN ACCESS
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-SECRET_KEY = os.getenv("SECRET_KEY")
-def myEnv():
-    print(f'MY ID IS: {TOKEN}')
-    print(f'MY SECRET KET IS: {SECRET_KEY}')
-if __name__ == "__main__":
-    myEnv
 
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logging on as {self.user}')
 
-
 intents = discord.Intents.default()
 intents.message_content = True
-
 
 client = Client(intents=intents)
 client.run('TOKEN')
